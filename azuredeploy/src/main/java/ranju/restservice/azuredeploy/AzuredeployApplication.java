48171e035f2f204143b6e127b0d18e412cbfb7ad
ranjitha-ko <75278517+ranjitha-ko@users.noreply.github.com>
2021-01-12 12:23:52 -0800

    initail commit

---
+++ azuredeploy/src/main/java/ranju/restservice/azuredeploy/AzuredeployApplication.java
@@ -0,0 +1,13 @@
+package ranju.restservice.azuredeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzuredeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzuredeployApplication.class, args);
+	}
+
+}
---
+++ azuredeploy/src/test/java/ranju/restservice/azuredeploy/AzuredeployApplicationTests.java
@@ -0,0 +1,13 @@
+package ranju.restservice.azuredeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzuredeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
